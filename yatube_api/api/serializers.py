6a0421f074c11e65b14a58f00fb35d468841e051
Григорий Юрченко <grigory.urchenko@gmail.com>
2022-07-27 01:33:31 +0300

    2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,24 +1,15 @@
 from rest_framework import serializers
+import datetime as dt
 from posts.models import Post, Group, Comment
 
 
+
 class PostSerializer(serializers.ModelSerializer):
-    character_quantity = serializers.SerializerMethodField()
-    publication_date = serializers.DateTimeField(source='pub_date', read_only=True)
-    group = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Group.objects.all(),
-        required=False
-    )
+    author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
-        fields = ('id', 'text', 'author', 'image',
-                  'character_quantity', 'publication_date', 'group')
-        read_only_fields = ('author',)
+        fields = ('id', 'text', 'author', 'image', 'pub_date')
         model = Post
-
-    def get_character_quantity(self, obj):
-        return len(obj.text)
 
 
 class GroupSerializer(serializers.ModelSerializer):
