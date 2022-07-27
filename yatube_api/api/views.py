27afe83867e09b4fd6d3d00a1d3551f974d8cdf5
Григорий Юрченко <grigory.urchenko@gmail.com>
2022-07-27 23:36:00 +0300

    27.07.22

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework import viewsets 
+from requests import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .permissions import AuthorOrReadOnly, ReadOnly
+
 
 from posts.models import Post, Comment, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
@@ -7,6 +12,16 @@ from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions() 
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -16,7 +31,10 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly, )
+    
     def get_queryset(self):
         post_id = self.kwargs.get("post_id")
         new_queryset = Comment.objects.filter(post=post_id)
         return new_queryset
+    
